d3e235981baea5c378aed771e867f2cb421d7d05
Federico Zivolo <fzivolo@quid.com>
2018-04-05 11:37:20 +0200

    build: simplified build, and ship cjs in lib

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,55 @@
 import babel from 'rollup-plugin-babel';
 import minify from 'rollup-plugin-babel-minify';
 
-export default {
+const common = {
   input: 'src/index.js',
-  plugins: [
-    babel({
-      babelrc: false,
-      presets: [['env', { modules: false }], 'stage-1', 'react'],
-      plugins: [
-        'external-helpers',
-        ['transform-react-remove-prop-types', { mode: 'wrap' }],
-      ],
-    }),
-    process.env.MINIFY ? minify() : false,
-  ].filter(Boolean),
-  external: ['react', 'react-dom', 'prop-types', 'popper.js'],
-  output: {
-    sourcemap: true,
-    globals: {
-      react: 'react',
-      'prop-types': 'PropTypes',
-      'popper.js': 'PopperJS',
-    },
+  external: [
+    'react',
+    'react-dom',
+    'popper.js',
+    'create-react-context',
+    'warning',
+  ],
+};
+
+const commonOutput = {
+  name: 'react-popper',
+  sourcemap: true,
+  globals: {
+    react: 'react',
+    'prop-types': 'PropTypes',
+    'popper.js': 'PopperJS',
+    'create-react-context': 'createContext',
+    warning: 'warning',
   },
 };
+
+const babelConfig = {
+  babelrc: false,
+  presets: [['env', { modules: false }], 'stage-1', 'react'],
+  plugins: [
+    'external-helpers',
+    ['transform-react-remove-prop-types', { mode: 'wrap' }],
+  ],
+};
+
+export default [
+  {
+    ...common,
+    plugins: [babel(babelConfig)],
+    output: {
+      ...commonOutput,
+      format: 'umd',
+      file: 'dist/react-popper.js',
+    },
+  },
+  {
+    ...common,
+    plugins: [babel(babelConfig), minify()],
+    output: {
+      ...commonOutput,
+      format: 'umd',
+      file: 'dist/react-popper.min.js',
+    },
+  },
+];
