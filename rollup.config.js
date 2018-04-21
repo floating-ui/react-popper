814128a1ae5eee0ff0cf82cac2e7ffbd6f98cef4
Mark Johnson <mark@techpivot.net>
2018-04-21 11:56:39 -0700

    fix: v0.x dist builds to ensure SSR works properly and popper.js is included in the dist builds.

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,34 @@
 import babel from 'rollup-plugin-babel';
 import minify from 'rollup-plugin-babel-minify';
+import replace from 'rollup-plugin-replace';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
   input: 'src/index.js',
   plugins: [
+    nodeResolve(),
     babel({
       babelrc: false,
       presets: [['env', { modules: false }], 'stage-2', 'react'],
       plugins: [
         'external-helpers',
-        ['transform-react-remove-prop-types', { mode: 'wrap' }],
       ],
+      env: {
+        production: {
+          plugins: [
+            ['transform-react-remove-prop-types', { removeImport: true }],
+          ],
+        },
+      },
     }),
-    process.env.MINIFY ? minify() : false,
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    process.env.NODE_ENV === 'production' ? minify({
+      comments: false,
+    }) : false,
   ].filter(Boolean),
-  external: ['react', 'react-dom', 'prop-types', 'popper.js'],
+  external: ['react', 'react-dom', 'prop-types'],
   output: {
     sourcemap: true,
     globals: {
