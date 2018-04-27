8a856ec556f17713dbf8b8d8c49a9d6f29a623c7
Gilad Gray <ggray@palantir.com>
2018-04-27 14:55:12 -0700

    add ReferenceChildrenProps, replace RefProps with type RefHandler

--- typings/react-popper.d.ts
+++ typings/react-popper.d.ts
@@ -2,36 +2,40 @@ import * as React from "react";
 import * as PopperJS from "popper.js";
 
 interface ManagerProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 export class Manager extends React.Component<ManagerProps, {}> { }
 
-interface RefProps {
-    ref: (ref: HTMLElement | null) => void;
+type RefHandler = (ref: HTMLElement | null) => void;
+
+interface ReferenceChildrenProps {
+  ref: RefHandler;
 }
 
 interface ReferenceProps {
-    children: (props: RefProps) => React.ReactNode;
+  children: (props: ReferenceChildrenProps) => React.ReactNode;
 }
 export class Reference extends React.Component<ReferenceProps, {}> { }
 
-interface ArrowProps extends RefProps {
-    style: React.CSSProperties;
+interface PopperArrowProps {
+  ref: RefHandler;
+  style: React.CSSProperties;
 }
 
-interface PopperChildrenProps extends RefProps {
-    arrowProps: ArrowProps;
-    outOfBoundaries: boolean | null;
-    placement: PopperJS.Placement;
-    scheduleUpdate: () => void;
-    style: React.CSSProperties;
+interface PopperChildrenProps {
+  arrowProps: PopperArrowProps;
+  outOfBoundaries: boolean | null;
+  placement: PopperJS.Placement;
+  ref: RefHandler;
+  scheduleUpdate: () => void;
+  style: React.CSSProperties;
 }
 
 interface PopperProps {
-    children: (props: PopperChildrenProps) => React.ReactNode;
-    eventsEnabled?: boolean;
-    modifiers?: PopperJS.Modifiers;
-    placement?: PopperJS.Placement;
-    positionFixed?: boolean;
+  children: (props: PopperChildrenProps) => React.ReactNode;
+  eventsEnabled?: boolean;
+  modifiers?: PopperJS.Modifiers;
+  placement?: PopperJS.Placement;
+  positionFixed?: boolean;
 }
 export class Popper extends React.Component<PopperProps, {}> { }
