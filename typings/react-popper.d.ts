1cc58450abd19acc7306228bdc323d890e0d1eaf
Gilad Gray <giladgray@gmail.com>
2018-04-26 18:11:54 -0700

    more interfaces in typescript typings
    split typescript types into more interfaces that can be used separately in app code

--- typings/react-popper.d.ts
+++ typings/react-popper.d.ts
@@ -2,32 +2,35 @@ import * as React from "react";
 import * as PopperJS from "popper.js";
 
 interface ManagerProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 export class Manager extends React.Component<ManagerProps, {}> { }
 
+interface RefProps {
+    ref: (ref: HTMLElement | null) => void;
+}
+
 interface ReferenceProps {
-  children: (props: ({
-    ref: (ref: HTMLElement | null) => void,
-  })) => React.ReactNode;
+    children: (props: RefProps) => React.ReactNode;
 }
 export class Reference extends React.Component<ReferenceProps, {}> { }
 
+interface ArrowProps extends RefProps {
+    style: React.CSSProperties;
+}
+
+interface PopperChildrenProps extends RefProps {
+    arrowProps: ArrowProps
+    placement: PopperJS.Placement;
+    scheduleUpdate: () => void;
+    style: React.CSSProperties;
+}
+
 interface PopperProps {
-  modifiers?: PopperJS.Modifiers;
-  placement?: PopperJS.Placement;
-  eventsEnabled?: boolean;
-  positionFixed?: boolean;
-  children: (props: ({
-    ref: (ref: HTMLElement | null) => void,
-    style: React.CSSProperties,
-    placement: ?PopperJS.Placement,
-    outOfBoundaries: boolean | null,
-    scheduleUpdate: () => void,
-    arrowProps: {
-      ref: (ref: HTMLElement | null) => void,
-      style: React.CSSProperties,
-    },
-  })) => React.ReactNode;
+    children: (props: PopperChildrenProps) => React.ReactNode;
+    eventsEnabled?: boolean;
+    modifiers?: PopperJS.Modifiers;
+    placement?: PopperJS.Placement;
+    positionFixed?: boolean;
 }
 export class Popper extends React.Component<PopperProps, {}> { }
