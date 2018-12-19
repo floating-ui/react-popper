8128f98f31c0ddf94ec6c78c6071f33d02b94f8c
Ronald J Kimball <rkimball@snapapp.com>
2018-12-19 09:54:28 -0600

    fix: avoid flow error for call to warning() (#260)
    With flow-typed, warning() expects its first argument to be a boolean.
    Added tests to ensure warning() is called correctly.

--- src/Reference.test.js
+++ src/Reference.test.js
@@ -1,5 +1,6 @@
 // @flow
 import React from 'react';
+import warning from 'warning';
 import { mount } from 'enzyme';
 
 // Public API
@@ -8,7 +9,13 @@ import { Reference } from '.';
 // Private API
 import { ManagerContext } from './Manager';
 
+jest.mock('warning');
+
 describe('Arrow component', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('renders the expected markup', () => {
     const setReferenceNode = jest.fn();
 
@@ -45,5 +52,46 @@ describe('Arrow component', () => {
       </div>
     );
     expect(setReferenceNode).toHaveBeenCalled();
+  });
+
+  it('warns when setReferenceNode is present', () => {
+    const setReferenceNode = jest.fn();
+
+    // HACK: wrapping DIV needed to make Enzyme happy for now
+    mount(
+      <div>
+        <ManagerContext.Provider
+          value={{
+            setReferenceNode,
+            referenceNode: undefined,
+          }}
+        >
+          <Reference>{({ ref }) => <div ref={ref} />}</Reference>
+        </ManagerContext.Provider>
+      </div>
+    );
+    expect(warning).toHaveBeenCalledWith(
+      true,
+      '`Reference` should not be used outside of a `Manager` component.'
+    );
+  });
+
+  it('does not warn when setReferenceNode is not present', () => {
+    // HACK: wrapping DIV needed to make Enzyme happy for now
+    mount(
+      <div>
+        <ManagerContext.Provider
+          value={{
+            referenceNode: undefined,
+          }}
+        >
+          <Reference>{({ ref }) => <div ref={ref} />}</Reference>
+        </ManagerContext.Provider>
+      </div>
+    );
+    expect(warning).toHaveBeenCalledWith(
+      false,
+      '`Reference` should not be used outside of a `Manager` component.'
+    );
   });
 });
