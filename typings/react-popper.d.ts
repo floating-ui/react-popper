137613d588e5ed9061c5ee0ec97c0a27ae73cf6f
Jonathan Fleckenstein <jonfleck88@gmail.com>
2020-10-06 11:20:54 -0400

    Update react-popper.d.ts
    update type to reflect actual types returned.

--- typings/react-popper.d.ts
+++ typings/react-popper.d.ts
@@ -76,7 +76,7 @@ export function usePopper<Modifiers>(
   }
 ): {
   styles: { [key: string]: React.CSSProperties };
-  attributes: { [key: string]: { [key: string]: string } };
+  attributes: { [key: string]: { [key: string]: string } | undefined };
   state: PopperJS.State | null;
   update: PopperJS.Instance['update'] | null;
   forceUpdate: PopperJS.Instance['forceUpdate'] | null;
